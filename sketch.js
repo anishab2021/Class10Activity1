ecebdc7b98b53b40300583ee1dcde798adedd7b9
anishab2021 <93690730+anishab2021@users.noreply.github.com>
2022-02-20 19:34:46 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var marks = [30,45,38,60,5];
+console.log(marks[0])
+
+function score_average() {
+  var sum = marks[0]+marks[1]+marks[2]+marks[3]+marks[4]
+  var avg = sum/marks.length
+  console.log(avg)
+}
+function setup() {
+  createCanvas(400, 400);
+  score_average()
+}
+
+function draw() {
+  background(220);
+}
